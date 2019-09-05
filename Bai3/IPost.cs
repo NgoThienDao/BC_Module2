6b7322274cbebc899df641980821f7fdf907c11f
NgoThienDao <50938056+NgoThienDao@users.noreply.github.com>
2019-09-05 15:21:42 +0700

    add

---
+++ Bai3/IPost.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KiemTra.Bai3
+{
+    public interface IPost
+    {
+        string Display();
+        void CalculatorRate();
+    }
+}
